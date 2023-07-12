ba49b549ab3c3a2b311a7775473ab462f5c1c77b
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-07-12 17:24:27 +0900

    fix: module not loaded correctly

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,11 @@ import * as vscode from 'vscode';
 import { colorize } from './colorize';
 
 export async function activate(context: vscode.ExtensionContext) {
-  await colorize();
-
   context.subscriptions.push(
     vscode.commands.registerCommand('auto-colorize.recolor', async () => {
       await colorize({ overwriteConfig: true });
-    })
+    }),
   );
+
+  await colorize();
 }
