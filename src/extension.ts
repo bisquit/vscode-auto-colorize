25393788527906b101f7441f9b8f63270808dcc0
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-07-13 10:41:29 +0900

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,6 @@ import { colorize } from './colorize';
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand('auto-colorize.recolor', async () => {
-      vscode.window.showInformationMessage('Recolorize');
       await colorize({ overwriteConfig: true });
     }),
   );
